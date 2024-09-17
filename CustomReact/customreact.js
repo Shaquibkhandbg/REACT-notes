7384ebc76a968f2d0f9e2126e9217479506d4101
Shaquibkhandbg <shaquibkhandbg@gmail.com>
2024-09-17 23:32:32 +0530

    CustomReact/cusotmreact.js

--- CustomReact/customreact.js
+++ CustomReact/customreact.js
@@ -1,22 +1,32 @@
-function customRender(reactElement, container){
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
+// Function to render a custom React-like element into a specified container
+function customRender(reactElement, container) {
+    // Create a new DOM element of the type specified in reactElement.type
+    const domElement = document.createElement(reactElement.type);
+    
+    // Set the inner HTML of the created element to the value specified in reactElement.children
+    domElement.innerHTML = reactElement.children;
 
-    container.appendChild(domElement)
+    // Set attributes on the element if they are provided in reactElement.props
+    // For anchor tags, we set href and target attributes
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+
+    // Append the newly created and configured element to the container element
+    container.appendChild(domElement);
 }
 
+// Define a simple React-like element
 const reactElement = {
-    type: 'a',
+    type: 'a', // Type of the element (anchor tag)
     props: {
-        href: 'https://google.com',
-        target: '_blank'
+        href: 'https://google.com', // Attribute for the hyperlink
+        target: '_blank' // Attribute to open the link in a new tab
     },
-    children: 'click me to visit google '
+    children: 'click me to visit google ' // Inner content of the element
 }
 
-const mainContainer = document.getElementById('root')
+// Get the DOM element with the id 'root' where we will render our custom element
+const mainContainer = document.getElementById('root');
 
-customRender(reactElement, mainContainer)
-
+// Call customRender to render reactElement into the mainContainer
+customRender(reactElement, mainContainer);
